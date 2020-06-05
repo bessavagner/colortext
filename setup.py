b1df4b0591c35c693959e604e8ab310144b7c7cf
Vagner Bessa <bessavagner@gmail.com>
2020-06-04 21:19:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Color you text terminal output",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bessavagner/colotext",
+    url="https://github.com/bessavagner/colortext",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
